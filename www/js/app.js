9a2eacb63ebfed34e9ac42452b0f04a3921f5440
Kuba Kuźma <kuba@jah.pl>
2014-04-26 20:12:34 +0200

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,17 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+    // put your routes here
+});
+
+App.IndexRoute = Ember.Route.extend({
+    model: function() {
+        return ["red", "yellow", "blue"];
+    }
+});
+
+ref = new Firebase("https://splitr.firebaseio.com");
+
+auth = new FirebaseSimpleLogin(ref, function(error, user) {
+    alert(user);
+});
